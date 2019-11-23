5a24357853c3299c2fa41ad3a87c1494d6f0a6c7
edson-mr <58115008+edson-mr@users.noreply.github.com>
2019-11-23 11:26:21 -0500

    Add files via upload

---
+++ Dos_en_Consola.java
@@ -0,0 +1,27 @@
+
+package Mendoza_Ramirez_Edson;
+
+import java.util.Scanner;
+
+public class Dos_en_Consola {
+    public static void main(String[] args) {
+       Scanner entrada=new Scanner(System.in);
+       int dni,resto;
+       String op="si";
+       while(op.equalsIgnoreCase("si")){
+       do{
+            System.out.print("Ingrese dni: ");
+             dni=entrada.nextInt();
+            if (String.valueOf(dni).length() == 8) {
+                String arreglo[] = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+                resto = dni % 23;
+                System.out.println("su NIF es: '"+arreglo[resto]+"'");
+            } else {
+                System.out.println("El dni debe contener 8 digitos");
+            }
+    }while(String.valueOf(dni).length() != 8);
+           System.out.print("Desea continuar(si) o Salir(cualquier letra): ");
+           op=entrada.next();
+    }
+    } 
+}
